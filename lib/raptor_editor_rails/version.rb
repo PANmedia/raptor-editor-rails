d5a58cc46c104f76cbaf74023c371aac33fb8e24
Mateusz Kozak <mateusz@mkozak.pl>
2012-11-19 19:47:42 +0100

    version bump

--- lib/raptor_editor_rails/version.rb
+++ lib/raptor_editor_rails/version.rb
@@ -1,6 +1,6 @@
 module RaptorEditorRails
     module Version
-        VERSION = "0.1.6"
-        RAPTOR_EDITOR_VERSION = "0.0.26-beta"
+        VERSION = "0.1.6-enyuka"
+        RAPTOR_EDITOR_VERSION = "0.0.30-beta"
     end
 end
